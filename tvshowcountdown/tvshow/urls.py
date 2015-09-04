93013bbd63cae832dae24c9abd7c33373e7ec1db
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2015-09-04 19:18:52 +0200

    added url and empty function for searching from index

--- tvshowcountdown/tvshow/urls.py
+++ tvshowcountdown/tvshow/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index_search$', views.index_search, name='index_search'),
 ]
--- tvshowcountdown/tvshow/views.py
+++ tvshowcountdown/tvshow/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
 
 def index(request):
     return render_to_response('index.html')
+
+def index_search(request):
+    return HttpResponse("hello")
