cb84d1925684322f4549fe6a5bbf46313eb28633
Adrian Moisey <adrian@changeover.za.net>
2015-09-06 17:38:45 +0200

    Calculate date here and also pass through some episode info to the html

--- tvshowcountdown/tvshow/views.py
+++ tvshowcountdown/tvshow/views.py
@@ -3,6 +3,8 @@ from django.core.urlresolvers import reverse
 
 from . import trakt
 
+import dateutil.parser
+
 def index(request):
     context = {}
     if "search_term" in request.GET:
@@ -17,12 +19,15 @@ def handle_search(request):
         context["search_results"] = trakt.search(request.GET["search_term"])
     return render_to_response('index.html', context)
 
+
 def countdown(request, slug_id):
     context = {}
-    time_later = trakt.get_next_episode(slug_id)
-    if time_later == None:
+    episode = trakt.get_next_episode(slug_id)
+    if episode['first_aired'] is None:
         context["error_message"] = "Could not get next episode"
     else:
+        timestamp = episode['first_aired']
+        time_later = dateutil.parser.parse(timestamp)
         countdown_timer = {}
         countdown_timer["year"] = time_later.year
         countdown_timer["month"] = time_later.month - 1 # Because js counts months from 0
@@ -31,6 +36,9 @@ def countdown(request, slug_id):
         countdown_timer["minute"] = time_later.minute
         countdown_timer["second"] = time_later.second
         context["countdown_timer"] = countdown_timer
+        show_info = {}
+        show_info["title"] = episode["title"]
+        show_info["season"] = episode["season"]
+        show_info["episode"] = episode["number"]
+        context["show_info"] = show_info
     return render_to_response('index.html', context)
-    
-    
