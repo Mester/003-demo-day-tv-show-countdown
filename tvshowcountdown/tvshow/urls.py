b80805fdd4171468eefedadcbbf64c2075c3efed
Adrian Moisey <adrian@changeover.za.net>
2015-09-09 12:57:10 +0200

    Rename countdown to info

--- tvshowcountdown/tvshow/urls.py
+++ tvshowcountdown/tvshow/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.search, name='index'),
     url(r'^search$', views.search, name='search'),
-    url(r'^results/(?P<slug_id>[a-zA-Z0-9\-]+)$', views.countdown, name='countdown'),
+    url(r'^info/(?P<slug_id>[a-zA-Z0-9\-]+)$', views.info, name='info'),
     url(r'^shows$', views.shows, name='shows')
 ]
